6d71e2b621c316fb712c9b5d3ac80d0ba61145c4
IsaacLera <isaac.lera@gmail.com>
2020-06-10 13:15:07 +0200

    inf generator

---
+++ infrastructureGenerator/README.md
@@ -0,0 +1,5 @@
+# Infrastructure generator
+
+It uses networkx library to generate the graph and includes nodes and links characteristics. Finally, it transform the graph in a JSON format compatible with multi-agent-policies project, and also, with YAFS.
+
+
--- infrastructureGenerator/generator.py
+++ infrastructureGenerator/generator.py
@@ -1,5 +1,17 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
+import json
+
+#Steps to create an Infrastructure
+
+
+# =============================================================================
+#  1
+#  THE NETWORK MODEL
+#  Note: Which model?
+# 
+# =============================================================================
 
 # Returns a 𝐺𝑛,𝑝 random graph, also known as an Erdős-Rényi graph or a binomial graph.
 G = nx.gnp_random_graph(10, 0.5, seed=10, directed=False)
@@ -50,3 +62,66 @@ pos = nx.kamada_kawai_layout(G)
 # pos = nx.spectral_layout(G)
 nx.draw(G, pos)
 plt.show()
+
+
+#Better vis. with Gephi
+nx.write_gexf(G,"sample.gexf")
+
+
+
+# =============================================================================
+#  2
+#  Network/Infrastructure attributes
+#  - Node capacity
+#  - Node speed
+#  - Links latency and bandwidth
+# =============================================================================
+
+
+# - Links latency and bandwidth
+valuesOne = dict(zip(G.edges(), np.ones(len(G.edges()))))
+print(valuesOne)
+nx.set_edge_attributes(G, name='BW', values=valuesOne)
+nx.set_edge_attributes(G, name='PR', values=valuesOne)
+
+# "HwReqs" node attribute,
+capacity_size = 6
+capacity = dict(zip(G.nodes(), np.ones(len(G.nodes()))*capacity_size))
+print(capacity)
+nx.set_node_attributes(G, name='HwReqs', values=capacity)
+
+
+# "IPT": 1000,
+ipt = 1000
+iptvalues= dict(zip(G.nodes(), np.ones(len(G.nodes()))*ipt))
+nx.set_node_attributes(G, name='IPT', values=iptvalues)
+
+# SHAPE node attribute "shape": "(4,2)",
+#if capacity is 6, then (3,3) > current version: manual
+values = (("(3,3)|"*len(G.nodes())).split("|"))[:-1]
+shapes = dict(zip(G.nodes(),values))
+nx.set_node_attributes(G, name='shape', values=shapes)
+
+# POS attribute - "pos": "(4,4)"
+loc = np.array(pos.values())
+minXvalue,maxXvalue = loc[:,0].min(),loc[:,0].max()
+minYvalue,maxYvalue = loc[:,1].min(),loc[:,1].max()
+future_draw_dim = 150
+normalizedX = ((future_draw_dim*(loc[:,0]-minXvalue)/(maxXvalue-minXvalue)).astype(int)
+normalizedY = (future_draw_dim*(loc[:,0]-minYvalue)/(maxYvalue-minYvalue)).astype(int)
+posTopo = dict(zip(G.nodes(),zip(normalizedX,normalizedY)))
+nx.set_node_attributes(G, name='pos', values=posTopo)
+
+
+# =============================================================================
+# 3
+# JSON GENERATOR
+# Note: YAFS support directaly the use of "G": t.G = G
+# =============================================================================
+
+net = {}
+net['entity']=nx.node_link_data(G)["nodes"]
+net['link']=nx.node_link_data(G)["links"]
+with open("networkDefinition.json","w") as f:
+    f.write(json.dumps(net))
+        
