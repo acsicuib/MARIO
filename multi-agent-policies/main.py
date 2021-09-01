02a62a5b1e9d7fb5c884be0c51b3041cd0b1cca4
IsaacLera <isaac.lera@gmail.com>
2021-09-01 09:25:25 +0200

    minor

--- multi-agent-policies/main.py
+++ multi-agent-policies/main.py
@@ -272,8 +272,9 @@ if __name__ == '__main__':
     # import os
     # print(os.getcwd())
 
-    fileName = "experiment_LSeed.json"
-    # fileName = "experiment_MSeedTEST.json" #TODO
+    fileName = "experiment_SSeed.json"
+    # fileName = "experiment_LSeed.json"
+    # fileName = "experiment_MSeedTEST.json"
     with open(fileName) as f:
         experiments = json.load(f)
 
